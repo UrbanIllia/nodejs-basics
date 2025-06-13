51f15cf02f99948c8d8b265b9f1d8e471a24b101
Urban <urban.mutagen@gmail.com>
2025-06-13 19:52:11 +0300

    add middlewares beter

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -1,4 +1,10 @@
-import { getAllStudents, getStudentById } from '../services/students.js';
+import {
+  createStudent,
+  deleteStudent,
+  getAllStudents,
+  getStudentById,
+  updateStudent,
+} from '../services/students.js';
 import createHttpError from 'http-errors';
 
 export const getStudentsController = async (req, res, next) => {
@@ -28,5 +34,63 @@ export const getStudentByIdController = async (req, res) => {
     status: 200,
     message: `Successfully found student with id ${studentId}!`,
     data: student,
+  });
+};
+export const createStudentController = async (req, res) => {
+  const student = await createStudent(req.body);
+
+  res.status(201).json({
+    status: 201,
+    message: `Successfully created a student!`,
+    data: student,
+  });
+};
+export const deleteStudentController = async (req, res, next) => {
+  const { studentId } = req.params;
+
+  const student = await deleteStudent(studentId);
+
+  if (!student) {
+    next(createHttpError(404, 'Student not found'));
+    return;
+  }
+
+  res.status(204).send();
+};
+
+export const upsertStudentController = async (req, res, next) => {
+  const { studentId } = req.params;
+
+  const result = await updateStudent(studentId, req.body, {
+    upsert: true,
+  });
+
+  if (!result) {
+    next(createHttpError(404, 'Student not found'));
+    return;
+  }
+
+  const status = result.isNew ? 201 : 200;
+
+  res.status(status).json({
+    status,
+    message: `Successfully upserted a student!`,
+    data: result.student,
+  });
+};
+
+export const patchStudentController = async (req, res, next) => {
+  const { studentId } = req.params;
+  const result = await updateStudent(studentId, req.body);
+
+  if (!result) {
+    next(createHttpError(404, 'Student not found'));
+    return;
+  }
+
+  res.json({
+    status: 200,
+    message: `Successfully patched a student!`,
+    data: result.student,
   });
 };
